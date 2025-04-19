491047352d9905ce0af2fb17f1aa862024a8b7e5
Tanishka Sharma <120589949+tanishka167@users.noreply.github.com>
2025-04-19 17:01:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,5 +19,5 @@ def calculate_cost():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=5000)
+''' if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0', port=5000)'''
